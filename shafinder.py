2fcffc37f0054424547248c3dc951462beeb21ab
Javier Rojas <46604653+javierrojas10@users.noreply.github.com>
2021-07-30 12:11:24 -0400

    fix: typo

--- shafinder.py
+++ shafinder.py
@@ -3,11 +3,10 @@ import string
 import random
 
 def encrypt_string(hash_string):
-    sha_signature = \
-        hashlib.sha256(hash_string.encode()).hexdigest()
+    sha_signature = hashlib.sha256(hash_string.encode()).hexdigest()
     return sha_signature
 
-def getAWord(size=6, chars=string.ascii_lowercase +string.digits):
+def getAWord(size=6, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def findSHA():
