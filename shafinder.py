e0b83a6394fb8ab1b4bdac9ef6fd1edfc1552980
Javier Rojas <46604653+javierrojas10@users.noreply.github.com>
2021-07-30 12:09:02 -0400

    Init Commit
    Init Commit: first deploy

---
+++ shafinder.py
@@ -0,0 +1,21 @@
+import hashlib
+import string
+import random
+
+def encrypt_string(hash_string):
+    sha_signature = \
+        hashlib.sha256(hash_string.encode()).hexdigest()
+    return sha_signature
+
+def getAWord(size=6, chars=string.ascii_lowercase +string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def findSHA():
+    while True:
+        word = getAWord()
+        hashed = encrypt_string(word)
+        if(hashed.find('b00da') == True):
+            print(word)
+            print(encrypt_string(word))
+            break
+findSHA()
